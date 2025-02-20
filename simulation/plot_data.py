83c1f4ee16e06f7946cda1ed371bd52623b2f66f
sunpeihan <sunpeihan@mail.ustc.edu.cn>
2025-02-20 18:18:27 +0800

    update

--- simulation/plot_data.py
+++ simulation/plot_data.py
@@ -1,4 +1,15 @@
 import pickle
-with open(f"linear_sum.pkl", "rb") as f:
-    res_dic = pickle.load(f)
-print(1)
+import matplotlib.pyplot as plt
+with open(f"sin_sum.pkl", "rb") as f:
+    dic = pickle.load(f)
+plt.figure()
+plt.plot(dic['qx'], label='nominal')
+plt.plot(dic['qx'] + 0.01, label='nominal + 0.01')
+plt.plot(dic['qx'] - 0.01, label='nominal - 0.01')
+plt.plot(dic['q_ref'], 'o', label='reference', markersize=1)
+plt.plot(dic['q_predicted'], 'v', label='predicted', markersize=1)
+plt.plot(dic['q_measured'], 's', label='measured', markersize=1)
+# plt.plot(dic['peak_detection'], '*', label='peak detection', markersize=1)
+plt.plot(dic['curve_fitting'], 'p', label='curve fitting', markersize=1)
+plt.legend()
+plt.show()
